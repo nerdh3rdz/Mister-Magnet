631347c672e52503c55bdec54bdc76d247e60496
James Francis Lorenzo <202001086@iacademy.edu.ph>
2022-03-27 19:20:13 +0800

    mapping etc

---
+++ Mister Magnet/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField]
+    private float moveSpeed = 5.0f;
+
+    [Header("Ground")]
+    [SerializeField]
+    private Transform groundPoint;
+    [SerializeField]
+    private float groundRadius = 0.2f;
+    [SerializeField]
+    private LayerMask groundLayers;
+
+    private Rigidbody2D playerRigidBody;
+    private SpriteRenderer spriteRenderer;
+    public GameObject playerController;
+    private Animator animator;
+
+    private float horizontalInput, verticalInput;
+
+    private bool facingRight = true;
+    private bool upRight = true;
+
+    void Start()
+    {
+        //We used the rigidbody of the parent so that the character will flip around the parent's pivot
+        //and not its own which is set to the bottom of the sprite.
+        playerRigidBody = playerController.GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("speed", Mathf.Abs(horizontalInput));
+        verticalInput = Input.GetAxisRaw("Vertical");
+        Flip(horizontalInput);
+        if(IsGrounded())
+            ReverseGravity(verticalInput);
+    }
+
+    private void FixedUpdate()
+    {
+        //move the player based on the physics update
+        float horizontalMovement = horizontalInput * moveSpeed;
+        playerRigidBody.velocity = new Vector2(horizontalMovement, playerRigidBody.velocity.y);
+    }
+
+    private void Flip(float movement)
+    {
+        //if we have positive input and not facing right (facing left)
+        // OR
+        //if we have negative input and we are facing right
+        if (movement > 0 && !facingRight || movement < 0 && facingRight)
+        {
+            //toggle the boolean
+            facingRight = !facingRight;
+            spriteRenderer.flipX = !facingRight;
+        }
+    }
+    private void ReverseGravity(float movement)
+    {
+        if (movement > 0 && upRight || movement < 0 && !upRight)
+        {
+            playerRigidBody.gravityScale = -playerRigidBody.gravityScale;
+            Vector3 theScale = playerController.transform.localScale;
+            theScale.y *= -1;
+            playerController.transform.localScale = theScale;
+            upRight = !upRight;
+        }
+    }
+//* IsGround
+    private bool IsGrounded()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundPoint.position, groundRadius, groundLayers);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)//make sure that the gameobject we are colliding is not our own
+                return true;
+        }
+        return false;
+    }
+//*/
+}
